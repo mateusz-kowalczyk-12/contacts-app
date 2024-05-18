663c7b216f44146d6ad36ea2b461cdcb14575630
Mateusz Kowalczyk <git.user@sspg.pl>
2024-05-19 01:42:48 +0200

    Ammend added authentication

---
+++ ContactsApp/ContactsApp_API/Migrations/20240518201114_ChangedPasswordHashToPassword.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactsApp_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedPasswordHashToPassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Contacts",
+                newName: "Password");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Contacts",
+                newName: "PasswordHash");
+        }
+    }
+}
